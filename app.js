873952ecedbb50664612ac5a1fc60a2c316211fc
gyr0tron <simmarkalsi@gmail.com>
2018-02-13 21:30:25 +0530

    modified methods

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
