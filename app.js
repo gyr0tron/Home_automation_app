d6ef44d6186cb67b2725ffabea49f3dcc01f859f
gyr0tron <simmarkalsi@gmail.com>
2018-02-13 21:10:15 +0530

    fixed axios header error

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.use(require("express-session")({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
